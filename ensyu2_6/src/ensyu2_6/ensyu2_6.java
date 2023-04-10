9d82b03fd10ce0e7717278d4f67ab491d8e1f673
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-10 10:51:50 +0900

    概要、コメントの不備、変数名の加筆・修正を行いました。

--- ensyu2_6/src/ensyu2_6/ensyu2_6.java
+++ ensyu2_6/src/ensyu2_6/ensyu2_6.java
@@ -14,7 +14,7 @@ public class ensyu2_6 {
 	
 	/*
 	 * 関数名：main
-	 * 概要：
+	 * 概要：底辺と高さを入力し、三角形の面積を表示する。
 	 * 引数：なし
 	 * 戻り値：なし
 	 * 作成者：S.Saito
@@ -22,7 +22,7 @@ public class ensyu2_6 {
 	 */
 	public static void main(String[] args) {
 		
-		// キーボードによる入力をおっこなうように指定する。mainの最初に置く。
+		// キーボードによる入力を行うように指定する。mainの最初に置く。
 		Scanner standardInput = new Scanner(System.in);
 		
 		// この後に三角形の面積を求めることを示す。
@@ -30,16 +30,16 @@ public class ensyu2_6 {
 		
 		// 底辺の値を入力するように示す。
 		System.out.print("底辺：");
-		// 底辺の値をBaseに入力する。
-		double Base = standardInput.nextDouble();
+		// 底辺の値を入力する。
+		double BaseNumber = standardInput.nextDouble();
 		
 		// 高さの値を入力するように示す。
 		System.out.print("高さ：");
-		// 高さの値をHeightに入力する。
-		double Height = standardInput.nextDouble();
+		// 高さの値を入力する。
+		double HeightNumber = standardInput.nextDouble();
 		
 		// 三角形の面積を入力した底辺と高さに対応して示す。
-		System.out.println("面積は" + (Base * Height / 2) + "です。");
+		System.out.println("面積は" + (BaseNumber * HeightNumber / 2) + "です。");
 		
 	}
 
