7f3310de4dfd1f8023e81bcf75be29c9eeb4d0b7
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-12 11:53:02 +0900

    演習4-23をコミットしました。

---
+++ Ensyu4_23/src/Ensyu4_23/Ensyu4_23.java
@@ -0,0 +1,63 @@
+// パッケージの宣言
+package Ensyu4_23;
+
+//入力操作を行うときにクラスの宣言の前に置く。
+import java.util.Scanner;
+
+/*
+ * クラス名：Ensyu4_23
+ * 概要：演習4-23クラス
+ * 作成者：S.Saito
+ * 作成日：2023.04.12
+ */
+public class Ensyu4_23 {
+	
+	/*
+	 * 関数名：main
+	 * 概要：入力した値が縦軸の段数になる数字ピラミッドを表示する。数字は縦軸の段数を表示する。
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：S.Saito
+	 * 作成日：2023.04.12
+	 */
+	public static void main(String[] args){
+		
+		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
+		Scanner standardInput = new Scanner(System.in);
+		
+		// 段数を入れる変数をつくる
+		int integerNumber = 0;
+		// 段数に0以下が入った場合入力しなおすためのループ。
+		do {
+			// 段数の入力を求めるように表示する。
+			System.out.print("段数：");
+			// 段数を入力する。
+			integerNumber = standardInput.nextInt();
+			// 段数が0以下だった場合分岐する。
+			if (integerNumber <= 0) {
+				// 再度入力を行うように促す。
+				System.out.println("もう一度入力してください。");
+			}
+		// 入力した値が0以下の時にループする。
+		} while ( integerNumber <= 0 );
+		
+		// ピラミッドの縦軸の段数を入力した値と同じにするループ。
+		for ( int firstNumber = 1 ; firstNumber <= integerNumber ; firstNumber++ )  {
+			// 「空白」を表示するループ。繰り返す回数を入力した値より1少ない値の回数から
+			// 縦軸の段数が変わるごとに1回ずつ減らしていく。
+			for ( int secondNumber = firstNumber ; secondNumber < integerNumber ; secondNumber++ )  {
+				// 「空白」を表示する。ループによって繰り返し表示する。
+				System.out.print(" ");
+			}
+			// 縦軸の段数の数字を表示するループ。繰り返す回数を1回から縦軸の段数が変わるごと2回ずつ増やしていく。
+			for ( int secondNumber = 1 ; secondNumber < 2*firstNumber ; secondNumber++ )  {
+				// 縦軸の数字を表示する。ループによって繰り返し表示する。
+				System.out.print(firstNumber);
+			}
+			// 1段分の出力が終わったら改行を行う。
+			System.out.println();
+		}
+	
+	}
+
+}
