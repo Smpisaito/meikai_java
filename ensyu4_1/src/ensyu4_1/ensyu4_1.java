92d7ed000ac97be087964a12993e9f547f624382
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-12 17:06:31 +0900

    演習4-1の修正をしました。

--- ensyu4_1/src/ensyu4_1/ensyu4_1.java
+++ ensyu4_1/src/ensyu4_1/ensyu4_1.java
@@ -24,30 +24,30 @@ public class ensyu4_1 {
 	public static void main(String[] args){
 		
 		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
-		Scanner StandardInput = new Scanner(System.in);
+		Scanner standardInput = new Scanner(System.in);
 		
 		// ループに用いる整数値をつくる。初期値は二度目以降と分けるために0を入れる。
-		int RetryNumber = 0;
+		int retryNumber = 0;
 		
 		// もう一度行うことを選んだ時に戻るループをつくる
 		do {
 			// 操作が二度目以降の場合のみ行う。
-			if ( RetryNumber == 1 )
+			if ( retryNumber == 1 )
 				// 前回のループでの繰り返しの確認と分けるための改行
 				System.out.println("");
 			
 			// 整数値の入力を行うことを示す。
 			System.out.print("整数値：");
 			// 整数値の入力を行う。
-			int IntegerNumber = StandardInput.nextInt();
+			int integerNumber = standardInput.nextInt();
 			
 			
 			// 整数値が正の時の分岐。
-			if ( IntegerNumber > 0 ) {
+			if ( integerNumber > 0 ) {
 				// 入力値が正であることを表示する。
 				System.out.println("入力した整数値は正です");
-						// 整数値が負の時の分岐
-			} else if ( IntegerNumber < 0 ) {
+				// 整数値が負の時の分岐
+			} else if ( integerNumber < 0 ) {
 				// 入力値が負であることを表示する
 				System.out.println("入力した整数値は負です");
 			// それ以外、整数値が0であるときの分岐
@@ -58,14 +58,18 @@ public class ensyu4_1 {
 			
 			// 結果と分けるための改行
 			System.out.println("");
+			// 再度操作を行うかの解答をするまでループを行う。
+			do {
+				// もう一度操作を行うかの確認を表示する。
+				System.out.print("もう一度操作を行いますか？   1…Yes / 0…No  ：");
+				// もう一度操作を行うかどうかを数値として入力する。
+				retryNumber = standardInput.nextInt();
+			// 入力した値が１か0ではないなら繰り返す。
+			} while ( retryNumber < 0 || retryNumber > 1 );
 			
-			// もう一度操作を行うかの確認を表示する。
-			System.out.print("もう一度操作を行いますか？   1…Yes / 0…No  ：");
-			// もう一度操作を行うかどうかを数値として入力する。
-			RetryNumber = StandardInput.nextInt();
 			
 		// もう一度行うことを選ぶとループする条件になるように設定する。
-		} while ( RetryNumber == 1 );
+		} while ( retryNumber == 1 );
 		
 	}
 
