dc5f2496ef406518f85d9af35ef5b2dbf4aebf71
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-12 18:07:29 +0900

    変数名の変更とそれに伴うコメントの変更をしました。

--- ensyu4_5/src/ensyu4_5/ensyu4_5.java
+++ ensyu4_5/src/ensyu4_5/ensyu4_5.java
@@ -24,28 +24,28 @@ public class ensyu4_5 {
 	public static void main(String[] args){
 		
 		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
-		Scanner StandardInput = new Scanner(System.in);
+		Scanner standardInput = new Scanner(System.in);
 		
 		// カウントダウンを行うことを説明する。
 		System.out.println("カウントダウンをします。");
 		
-		// カウントダウンに用いる変数としてxをつくる。
-		int x;
+		// カウントダウンに用いる変数として元のプログラムのxの代わりの変数integerNumberをつくる。
+		int integerNumber;
 		
 		// 入力した値が正の整数値になるまでループする。
 		do {
 			// 正の整数値を入力することを示す。
 			System.out.print("正の整数値：");
 			// 整数値の入力を行う。
-			x = StandardInput.nextInt();
+			integerNumber = standardInput.nextInt();
 		// 入力された値が正はない整数値(0、負の整数値)であるときを条件にする。
-		} while ( x <= 0 );
+		} while ( integerNumber <= 0 );
 		
-		// 本来はデクリメントを用いて変数xが0未満になるまで繰り返す。
-		while ( x >= 0 )
-			// [明解Java List4-5]におけるデクリメントを(x--)を(--x)に変更したことで減少前ではなく減少後の値が表示されるようになり、
+		// 本来はデクリメントを用いて変数integerNumberが0未満になるまで繰り返す。
+		while ( integerNumber >= 0 )
+			// [明解Java List4-5]におけるデクリメントを(x--)を(--x)に変更したことで(本プログラム内ではintegerNumber)減少前ではなく減少後の値が表示されるようになり、
 			// 本来は入力値から0までのところ、入力値より1減じた値から-1まで出力してしまっている。
-			System.out.println(--x);
+			System.out.println(--integerNumber);
 	}
 
 }
