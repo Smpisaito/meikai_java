3409e9c69ca85ab1fd158de19bbe8da7f216f5db
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-12 17:48:49 +0900

    変数名の修正を行いました。

--- ensyu4_3/src/ensyu4_3/ensyu4_3.java
+++ ensyu4_3/src/ensyu4_3/ensyu4_3.java
@@ -18,40 +18,40 @@ public class ensyu4_3 {
 	 * 引数：なし
 	 * 戻り値：なし
 	 * 作成者：S.Saito
-	 * 作成日：
+	 * 作成日：2023.04.12
 	 */
 	public static void main(String[] args){
 	
 		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
-		Scanner StandardInput = new Scanner(System.in);
+		Scanner standardInput = new Scanner(System.in);
 		
 		
 		
 		// 一つ目の整数値の入力を行うことを示す
 		System.out.print("整数A：");
 		// 一つ目の整数値の入力を行う。
-		int FirstNumber = StandardInput.nextInt();
+		int firstNumber = standardInput.nextInt();
 		
 		// 二つ目の整数値の入力を行うことを示す。
 		System.out.print("整数B：");
 		// 二つ目の整数値の入力を行う。
-		int SecondNumber = StandardInput.nextInt();
+		int secondNumber = standardInput.nextInt();
 		
 		
 		
 		// 一つ目の整数値の方が大きい場合の分岐。
-		if ( FirstNumber > SecondNumber ) {
+		if ( firstNumber > secondNumber ) {
 			
 			//　二つ目の整数値の値から1ずつ表示する値を大きくし、表示した値が一つ目の整数値と一致したときに終わるループをつくる。
 			do {
 				
 				// 数字を表示する。数字の間がわかるように空白も入れる。
-				System.out.print( SecondNumber + " ");
+				System.out.print( secondNumber + " ");
 				// 表示した値に1を加える。
-				SecondNumber++;
+				secondNumber++;
 				
 			// 二つ目の数字の変数はループ内ですでに1加えているので、一つ目の値を超えた時にループは解除する。
-			} while ( SecondNumber <= FirstNumber );
+			} while ( secondNumber <= firstNumber );
 			
 		// 二つ目の整数値の方が大きい場合の分岐(二つの整数値が同じ場合も含む)。
 		} else {
@@ -60,12 +60,12 @@ public class ensyu4_3 {
 			do {
 			
 				// 数字を表示する。数字の間にはわかるように空白も入れる
-				System.out.print( FirstNumber + " ");
+				System.out.print( firstNumber + " ");
 				// 表示した値に1を加える。
-				FirstNumber++;
+				firstNumber++;
 			
 				// 一つ目の数字の変数はループ内ですでに1加えているので、二つ目の値を超えた時にループは解除する。
-			} while ( SecondNumber >= FirstNumber );
+			} while ( secondNumber >= firstNumber );
 			
 		}
 	}
