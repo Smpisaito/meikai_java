86045bb58805cfb12dc2b08be10c71246b324ce6
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-13 10:49:26 +0900

    初期値の追加と表示する文字の加筆を行いました。

--- Ensyu4_13/src/Ensyu4_13/Ensyu4_13.java
+++ Ensyu4_13/src/Ensyu4_13/Ensyu4_13.java
@@ -24,15 +24,15 @@ public class Ensyu4_13 {
 		
 		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
 		Scanner standardInput = new Scanner(System.in);
-		// 入力する変数をつくる。
-		int integerNumber;
+		
 		// これから行うことを示す。
 		System.out.println("1から入力した正の整数値までの和を求める。");
-		
+		// 入力する変数をつくる。
+		int integerNumber = 0;
 		// 正の整数値が入力されるまでループする。
 		do {
 			// 整数値の入力を行うことを示す。
-			System.out.print("整数値：");
+			System.out.print("正の整数値：");
 			// 整数値の入力を行う。
 			integerNumber = standardInput.nextInt();
 		//入力した値が正ではないときループを行う。
@@ -40,7 +40,7 @@ public class Ensyu4_13 {
 		
 		
 		// 和を入れる変数sumNumbrを作る。初期値は0にする。
-		int sumNumber =0;
+		int sumNumber = 0;
 		// 入力した値から1まで変数sumNumberに和を入れるためのループをつくる。
 		for(int loopNumber = 1; loopNumber <= integerNumber ;loopNumber++) {
 			// 入力した値から1までの数字を加算する。
