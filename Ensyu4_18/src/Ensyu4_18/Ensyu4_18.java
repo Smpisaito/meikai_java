84dc23d08bd8ba8f547872d8c42095ae672ccfe7
Smpisaito <ddt.smpi.saito@gmail.com>
2023-04-13 11:41:49 +0900

    変数の入力に0以下が入力された際にループする。

--- Ensyu4_18/src/Ensyu4_18/Ensyu4_18.java
+++ Ensyu4_18/src/Ensyu4_18/Ensyu4_18.java
@@ -24,12 +24,17 @@ public class Ensyu4_18 {
 		
 		// 入力をキーボードに指定する。main内で入力操作を行うコードより前に置く。
 		Scanner standardInput = new Scanner(System.in);
+		// 入力するための変数をつくる。
+		int integerNumber = 0;
 		
-		// 正の整数値の入力を示すように表示する。
-		System.out.print("正の整数値");
-		// 正の整数値を入力する。
-		int integerNumber = standardInput.nextInt();
-		
+		// 0以下が入力された場合
+		do {
+			// 正の整数値の入力を示すように表示する。
+			System.out.print("正の整数値：");
+			// 正の整数値を入力する。
+			integerNumber = standardInput.nextInt();
+		// 入力した値が0以下の時ループする。
+		} while (integerNumber <= 0 );
 		// １から入力した値までそれぞれの二乗を求めるループをつくる。
 		for ( int loopNumber = 1; loopNumber <= integerNumber; loopNumber++ ) {
 			// 元の数とそれに対応する二乗が何かわかるように表示する。
